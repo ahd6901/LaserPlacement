1d376fa39b76742db206cf47076c592d875c0cd7
sm9943 <sm9943@rit.edu>
2020-04-21 00:55:10 -0400

    Initial test commit

---
+++ src/lasers/model/Safe.java
@@ -0,0 +1,461 @@
+package lasers.model;
+
+import java.util.ArrayList;
+
+
+/**
+ * The Model of the LasersPTUI project to represent the "safe" component of the game.
+ * A safe whose floors are represented by a 2d array. Each floor tile is either covered by
+ * a Pillar, Laser, or a laser beam.
+ *
+ * @author : Amy Do
+ * @author : Shubhang Mehrotra
+ *
+ * last modified : 04/17/2020
+ */
+public class Safe {
+    private int row, column;                // co-ordinates of a floor tile
+    private String[][] safe;                //a 2D array to represent the safe floor
+    private Integer[][] laserBeamCount;     // keeps count of the numbers of laser-beams on a tile
+
+
+    /**
+     * create the safe object from given dimensions.
+     * @param row- of the safe
+     * @param column- of the safe
+     */
+    public Safe(int row, int column) {
+        this.row = row;
+        this.column = column;
+        this.safe = new String[row][column];
+        this.laserBeamCount = new Integer[row][column];
+
+        for (int i = 0; i < this.column; i++) {
+            for (int j = 0; j < this.row; j++) {
+                this.laserBeamCount[i][j] = 0;
+            }
+        }
+    }
+
+
+    /**
+     * add a Laser at the required coordinates and make the laser beam
+     * @param r - row
+     * @param c - column
+     */
+    public void addLaser(int r, int c) {
+
+        if (r < this.row && c < this.column) {
+            String target = safe[r][c];
+            if (target.equals(".") || target.equals("*")) {
+                safe[r][c] = "L";
+
+                //loop to draw east laser-beam
+                for (int i = c + 1; i < this.column; i++) {
+                    if ((isPillar(safe[r][i]) || safe[r][i].equals("L"))) {
+                        break;
+                    } else {
+                        safe[r][i] = "*";
+                        laserBeamCount[r][i]++;
+                    }
+                }
+
+                //loop to draw west laser-beam
+                for (int i = c - 1; i >= 0; i--) {
+                    if ((isPillar(safe[r][i]) || safe[r][i].equals("L"))) {
+                        break;
+                    } else {
+                        safe[r][i] = "*";
+                        laserBeamCount[r][i]++;
+                    }
+                }
+
+                //loop to draw north laser-beam
+                for (int i = r - 1; i >= 0; i--) {
+                    if ((isPillar(safe[i][c]) || safe[i][c].equals("L"))) {
+                        break;
+                    } else {
+                        safe[i][c] = "*";
+                        laserBeamCount[i][c]++;
+                    }
+                }
+
+                //loop to draw south laser-beam
+                for (int i = r + 1; i < this.column; i++) {
+                    if ((isPillar(safe[i][c]) || safe[i][c].equals("L"))) {
+                        break;
+                    } else {
+                        safe[i][c] = "*";
+                        laserBeamCount[i][c]++;
+                    }
+                }
+                System.out.println(String.format("Laser added at: (%d, %d)", r, c));
+            } else {
+                System.out.println(String.format("Error adding laser at: (%d, %d)", r, c));
+            }
+        } else {
+            System.out.println(String.format("Error adding laser at: (%d, %d)", r, c));
+        }
+        displaySafe();
+    }
+
+
+    /**
+     * method to check the validity of the given pillar
+     * @param tile : a tile in a safe
+     * @return : true : if tile is valid
+     * false: otherwise
+     */
+    private boolean isPillar(String tile) {
+        switch (tile) {
+            case "X":
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * remove a laser at given coordinates
+     *
+     * @param r row
+     * @param c column
+     */
+    public void removeLaser(int r, int c) {
+        if (r < this.row && c < this.column) {
+            String target = safe[r][c];
+            if (target.equals("L")) {
+                if(laserBeamCount[r][c] > 0) {
+                    safe[r][c] = "*";
+                }
+                else {
+                    safe[r][c] = ".";
+                }
+
+                //loop to remove east laser-beam
+                for (int i = c + 1; i < this.column; i++) {
+                    if ((isPillar(safe[r][i]) || safe[r][i].equals("L"))) {
+                        break;
+                    } else {
+                        laserBeamCount[r][i]--;
+                        if (laserBeamCount[r][i] == 0) {
+                            safe[r][i] = ".";
+                        }
+                    }
+                }
+
+                //loop to remove west laser-beam
+                for (int i = c - 1; i >= 0; i--) {
+                    if ((isPillar(safe[r][i]) || safe[r][i].equals("L"))) {
+                        break;
+                    } else {
+                        laserBeamCount[r][i]--;
+                        if (laserBeamCount[r][i] == 0) {
+                            safe[r][i] = ".";
+                        }
+                    }
+                }
+
+                //loop to remove north laser-beam
+                for (int i = r - 1; i >= 0; i--) {
+                    if ((isPillar(safe[i][c]) || safe[i][c].equals("L"))) {
+                        break;
+                    } else {
+                        laserBeamCount[i][c]--;
+                        if (laserBeamCount[i][c] == 0) {
+                            safe[i][c] = ".";
+                        }
+                    }
+                }
+
+                //loop to remove south laser-beam
+                for (int i = r + 1; i < this.column; i++) {
+                    if ((isPillar(safe[i][c]) || safe[i][c].equals("L"))) {
+                        break;
+                    } else {
+                        laserBeamCount[i][c]--;
+                        if (laserBeamCount[i][c] == 0) {
+                            safe[i][c] = ".";
+                        }
+                    }
+                }
+                System.out.println(String.format("Laser removed at: (%d, %d)", r, c));
+            } else {
+                System.out.println(String.format("Error removing laser at: (%d, %d)", r, c));
+            }
+        } else {
+            System.out.println(String.format("Error removing laser at: (%d, %d)", r, c));
+        }
+        displaySafe();
+    }
+
+    /**
+     * method to display the safe to standard output
+     */
+    public void displaySafe() {
+        StringBuilder str = new StringBuilder();
+        str.append(" ");
+        // build the column numbers
+        for (int col = 0; col < this.column; ++col) {
+            str.append(" ").append(col);
+        }
+        str.append("\n").append(" ");
+        str.append(" _".repeat(Math.max(0, this.column)));
+        str.append("\n");
+
+
+        // build the rows with number and values
+        for (int row = 0; row < this.row; ++row) {
+            str.append(row).append("|");
+
+            //print each line [row fixed, col++]
+            for (int col = 0; col < this.row; ++col) {
+                str.append(this.safe[row][col]).append(" ");
+            }
+            str.append("\n");
+        }
+        System.out.println(str);
+    }
+
+    /**
+     * method to add a tile in the safe
+     *
+     * @param str    : tile
+     * @param row    : row
+     * @param column : column
+     */
+    public void addTile(String str, int row, int column) {
+        this.safe[row][column] = str;
+    }
+
+    /**
+     * method to check the validity of a tile
+     *
+     * @param tile : a tile in a safe
+     * @return : true : if tile is valid
+     * false: otherwise
+     */
+    private boolean isTile(String tile) {
+        switch (tile) {
+            case "*":
+            case "L":
+            case "X":
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+                return true;
+            default:
+                return false;
+        }
+    }
+
+
+    /**
+     * displays the status message that indicates whether the safe is valid or not
+     *  + each tile must either be a pillar, beam or laser beam
+     *  + Number of Lasers surrounding a Pillar must equal the number that Pillar's specified.
+     *  + 2 lasers cannot be in the same row/column without a pillar between them
+     */
+    public void verify() {
+        boolean allCorrect = true;
+        for (int r = 0; r < this.row; r++) {
+            for (int c = 0; c < this.column; c++) {         //loop to iterate over the safe
+                String tile = safe[r][c];
+
+                //check if's the tile is covered or not
+                if (isTile(tile)) {
+
+                    //if tile is a pillar, do the check of  surrounding lasers.
+                    boolean PillarNum = true;
+                    if (isPillar(tile)) {
+                        PillarNum = checkPillarLasers(r, c);
+                        if (!PillarNum) {
+                            System.out.println(String.format("Error verifying at: (%d, %d)", r, c));
+                            allCorrect = false;
+                        }
+                    }
+
+                    //if is a laser, do the collision check (N-E-S-W)
+                    if (tile.equals("L")) {
+                        if (!LaserCollisonNorthCheck(r, c) || !LaserCollisonEastCheck(r, c) ||
+                                !LaserCollisonSouthCheck(r, c) || !LaserCollisonWestCheck(r, c)) {
+                            allCorrect = false;
+                            System.out.println(String.format("Error verifying at: (%d, %d)", r, c));
+                        }
+                    }
+                } else {
+                    System.out.println(String.format("Error verifying at: (%d, %d)", r, c));
+                    allCorrect = false;
+                }
+            }
+        }
+        if (allCorrect) {
+            System.out.println("Safe is fully verified!");
+        }
+    }
+
+    /**
+     * checks if the number of Lasers surrounding a Pillar equal the number that Pillar's specified or not
+     * @param r- row coordinate of the pillar
+     * @param c- column coordinate of the pillar
+     * @return true if match, otherwise false.
+     */
+    public boolean checkPillarLasers(int r, int c) {
+        boolean result = true;
+        ArrayList<String> LaserList = new ArrayList<>();
+
+        String pillar = safe[r][c];
+        // if (pillar.equals("L")
+        //only add inbound tiles to the list.
+        if (inBound(r, c - 1)) {
+            LaserList.add(safe[r][c - 1]);
+        }
+        if (inBound(r, c + 1)) {
+            LaserList.add(safe[r][c + 1]);
+        }
+        if (inBound(r - 1, c)) {
+            LaserList.add(safe[r - 1][c]);
+        }
+        if (inBound(r + 1, c)) {
+            LaserList.add(safe[r + 1][c]);
+        }
+
+        int count = 0;
+        for (String i : LaserList) { //
+            if (i.equals("L")) {
+                count++;
+            }
+        }
+        //no need to check for "X"
+        if (pillar.equals("X")) {
+            result = true;
+        } else {
+            int test = Integer.parseInt(pillar);
+            if (test != count) {
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * checks if the given coordinates are within the bound or not
+     *
+     * @param r-row to coordinate be checked
+     * @param c-column coordinate to be checked
+     * @return true of inbound, false if out of bound
+     */
+    public boolean inBound(int r, int c) {
+        return r >= 0 && r < row && c >= 0 && c < column;
+    }
+
+    /**
+     * Check if collision occurred on the East side of the Laser
+     * @param r- row of the Laser
+     * @param c- column of the laser
+     * @return true of no collision, otherwise false.
+     */
+    public boolean LaserCollisonEastCheck(int r, int c) {
+        for (int i = c + 1; i < this.column; i++) {
+
+            if (isPillar(safe[r][i])) {
+                return true;
+            }
+            //if encounter another Laser
+            if (safe[r][i].equals("L")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * Check if collision occurred on the East side of the Laser
+     * @param r- row of the Laser
+     * @param c- column of the laser
+     * @return true of no collision, otherwise false.
+     */
+    public boolean LaserCollisonWestCheck(int r, int c) {
+        for (int i = c - 1; i >= 0; i--) {
+            if (isPillar(safe[r][i])) {
+                return true;
+            }
+
+            //if encounter another Laser
+            if (safe[r][i].equals("L")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check if collision occurred on the East side of the Laser
+     * @param r- row of the Laser
+     * @param c- column of the laser
+     * @return true of no collision, otherwise false.
+     */
+    public boolean LaserCollisonNorthCheck(int r, int c) {
+
+        for (int i = r - 1; i >= 0; i--) {
+            if (isPillar(safe[i][c])) {
+                return true;
+            }
+            //if encounter another Laser
+            if (safe[i][c].equals("L")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * Check if collision occurred on the East side of the Laser
+     * @param r- row of the Laser
+     * @param c- column of the laser
+     * @return true of no collision, otherwise false.
+     */
+    public boolean LaserCollisonSouthCheck(int r, int c) {
+        for (int i = r + 1; i < this.column; i++) {
+            if (isPillar(safe[i][c])) {
+                return true;
+            }
+
+
+            if (safe[i][c].equals("L")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * method to print out the valid user commands for the functioning of the PTUI.
+     */
+    public void help() {
+        System.out.println("        a|add r c: Add laser to (r,c)\n" +
+                "        d|display: Display safe\n" +
+                "        h|help: Print this help message\n" +
+                "        q|quit: Exit program\n" +
+                "        r|remove r c: Remove laser from (r,c)\n" +
+                "        v|verify: Verify safe correctness ");
+    }
+
+    /**
+     * method to quit the program
+     */
+    public void quit() {
+        System.exit(0);
+    }
+
+}
--- src/lasers/ptui/LasersPTUI.java
+++ src/lasers/ptui/LasersPTUI.java
@@ -1,6 +1,10 @@
 package lasers.ptui;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 import lasers.model.LasersModel;
 import lasers.model.ModelData;
@@ -45,3 +49,237 @@ public class LasersPTUI implements Observer<LasersModel, ModelData> {
         // TODO
     }
 }
+//package lasers.ptui;
+//
+//        import lasers.model.Safe;
+//
+//        import java.io.BufferedReader;
+//        import java.io.FileNotFoundException;
+//        import java.io.FileReader;
+//        import java.io.IOException;
+//        import java.util.Scanner;
+//
+///**
+// * Class to represent the view and controller of the project
+// * This program reads the user arguments for the safe file, and input file. After reading the safe file it constructs
+// * the safe, and allow users to input their commands which will then be processed and display
+// * the result to the console.
+// *
+// * @author : Amy Do
+// * @author : Shubhang Mehrotra
+// *
+// * last modified : 04/17/2020
+// */
+//public class LasersPTUI {
+//    private Safe s;
+//
+//    /**
+//     * constructor, handling arguments from the command line.
+//     * @param args command line
+//     * @throws IOException when file not found
+//     */
+//    public LasersPTUI(String[] args) throws IOException {
+//        try {
+//            if (args.length == 1) {
+//                readOneArgument(args[0]);   //safeFile
+//            } else if (args.length == 2) {
+//                readTwoArguments(args[0], args[1]); //safeFile, inputFile
+//            }
+//        } catch (IOException io) {
+//            System.out.println("File cannot found exception");
+//        }
+//    }
+//
+//
+//    /**
+//     * Create the Safe, and process commands from user input
+//     * @param safeFile to create the safe from
+//     * @throws IOException when file can't be found.
+//     */
+//    public void readOneArgument(String safeFile) throws IOException {
+//        //construct the safe
+//        readSafeFile(safeFile);
+//
+//        //get user input.
+//        System.out.print(">");
+//        Scanner in = new Scanner(System.in);
+//        while (in.hasNextLine()) {
+//            processUserCommand(in.nextLine());  //process input
+//            System.out.print(">");              //prompt input
+//        }
+//    }
+//
+//    /**
+//     * reads safe file to construct the safe object
+//     * @param safeFile-contains information to construct safe object
+//     * @throws IOException- when file is not found
+//     */
+//    public void readSafeFile(String safeFile) throws IOException {
+//
+//        BufferedReader buff = new BufferedReader(new FileReader(safeFile));
+//        String firstLine = buff.readLine();
+//        String test;
+//        String[] list = firstLine.split(" ");
+//
+//        //construct the safe from (row, column)
+//        int row = Integer.parseInt(list[0]);
+//        int column = Integer.parseInt(list[1]);
+//        s = new Safe(row, column);
+//
+//
+//        //process each row of the safeFile
+//        for (int i = 0; i <= row; i++) {
+//            test = buff.readLine();
+//            processSafeLine(test, i);
+//        }
+//        s.displaySafe();
+//
+//    }
+//
+//    /**
+//     * construct 2d array representation of the safe
+//     *
+//     * @param commandLine : file with shelf
+//     * @param row         : the row to construct
+//     */
+//    public void processSafeLine(String commandLine, int row) {
+//        String[] ls = commandLine.split(" ");
+//        int column = 0;
+//        for (String str : ls) {
+//            switch (str) {
+//                case ".":
+//                    s.addTile(".", row, column);
+//                    column++;
+//                    break;
+//                case "X":
+//                    s.addTile("X", row, column);
+//                    column++;
+//                    break;
+//                case "0":
+//                    s.addTile("0", row, column);
+//                    column++;
+//                    break;
+//                case "1":
+//                    s.addTile("1", row, column);
+//                    column++;
+//                    break;
+//                case "2":
+//                    s.addTile("2", row, column);
+//                    column++;
+//                    break;
+//                case "3":
+//                    s.addTile("3", row, column);
+//                    column++;
+//                    break;
+//                case "4":
+//                    s.addTile("4", row, column);
+//                    column++;
+//                    break;
+//            }
+//        }
+//    }
+//
+//    /**
+//     * Processes each user command, and produce the action accordingly.
+//     * @param command to follow.
+//     */
+//    public void processUserCommand(String command) {
+//        String[] list = command.split(" ");
+//        char cmd = list[0].charAt(0);
+//        if (cmd == 'a') {
+//            if ((list.length == 3)) {
+//                int row = Integer.parseInt(list[1]);
+//                int column = Integer.parseInt(list[2]);
+//                if (list[1].isEmpty() || list[2].isEmpty() || row < 0 || column < 0) {
+//                    System.out.println("Incorrect coordinates: " + command);
+//                }else{
+//                    s.addLaser(row, column);
+//                }
+//            } else {
+//                System.out.println("Incorrect coordinates: " + command);
+//            }
+//
+//        } else if (cmd == 'd') {
+//            s.displaySafe();
+//        } else if (cmd == 'h') {
+//            s.help();
+//        } else if (cmd == 'q') {
+//            s.quit();
+//        } else if (cmd == 'v') {
+//            s.verify();
+//        } else if (cmd == 'r') {
+//            if ((list.length == 3)) {
+//                int row = Integer.parseInt(list[1]);
+//                int column = Integer.parseInt(list[2]);
+//                if (list[1].isEmpty() || list[2].isEmpty() || row < 0 || column < 0) {
+//                    System.out.println("Incorrect coordinates: (" + command + ")");
+//                }else{
+//                    s.removeLaser(row, column);
+//                }
+//            } else {
+//                System.out.println("Incorrect coordinates: (" + command+ ")");
+//            }
+//        } else {
+//            System.out.println("Unrecognized command: " + command);
+//        }
+//    }
+//
+//    /**
+//     * creates the board from the safe file, then handles commands from the input file
+//     * @param safeFile- to create the board from
+//     * @param inputFile- to read the commands from
+//     * @throws IOException when file is not found
+//     */
+//    public void readTwoArguments(String safeFile, String inputFile) throws IOException {
+//        //construct the safe
+//        readSafeFile(safeFile);
+//        //process inputFile
+//        readInputFile(inputFile);
+//        //get user input.
+//        System.out.print(">");
+//        Scanner in = new Scanner(System.in);
+//        while (in.hasNextLine()) {
+//            processUserCommand(in.nextLine());  //process input
+//            System.out.print(">");              //prompt input
+//        }
+//    }
+//
+//
+//    /**
+//     * reads input-file line by line until none is left.
+//     * @param inputFile to read input from
+//     * @throws IOException when file cannot be found.
+//     */
+//    public void readInputFile(String inputFile) throws IOException {
+//        try {
+//            BufferedReader buff = new BufferedReader(new FileReader(inputFile));
+//            String line;
+//
+//            //read each line, until none is left.
+//            while ((line = buff.readLine()) != null) {
+//                processUserCommand(line);
+//            }
+//        } catch (FileNotFoundException fnf) {
+//            System.out.println("File cannot be found!");
+//        } catch (IOException e) {
+//            System.out.println("IOException");
+//        }
+//    }
+//
+//
+//    /**
+//     * The main method to handle user input
+//     *
+//     * @param args command line arguments
+//     */
+//    public static void main(String[] args) throws FileNotFoundException, IOException {
+//        // check sanity of input
+//        if (args.length < 1 || args.length > 2) {
+//            System.out.println("Usage: java LasersPTUI safe-file [input]");
+//        } else {
+//            new LasersPTUI(args);
+//        }
+//    }
+//
+//
+//}
